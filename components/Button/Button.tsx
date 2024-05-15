a807db019defc5d234faa59b3154a2475bbb38fe
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-05-14 22:24:33 -0300

    refactor: add bg-primary on class name

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -6,7 +6,7 @@ const Button = ({ children, className }: ButtonProps) => {
   return (
     <button
       className={`
-      bg-[#2D5BFF] text-white rounded-[8px] px-[32px] py-[12px] font-extrabold
+        bg-primary text-white rounded-[8px] px-[32px] py-[12px] font-extrabold
         ${className}
         `}
     >
